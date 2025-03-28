7b8d6856940b97cc0aa9c16c7c841ae9343115ae
sterb <sterben8912@gmail.com>
2025-03-28 13:55:12 +0500

    init

---
+++ Pyaterochka/GameContoller.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace TopDownGame;
+
+public class GameController
+{
+    private GameModel model;
+    private GameView view;
+
+    public GameController()
+    {
+        model = new GameModel();
+        view = new GameView(model);
+    }
+
+    public void LoadContent(Microsoft.Xna.Framework.Content.ContentManager content)
+    {
+        view.LoadContent(content);
+    }
+
+    public void Update(GameTime gameTime)
+    {
+        model.Update();
+    }
+
+    public void Draw(SpriteBatch spriteBatch)
+    {
+        view.Draw(spriteBatch);
+    }
+}
---
+++ Pyaterochka/GameView.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace TopDownGame;
+
+public class GameView
+{
+    private GameModel model;
+    private Texture2D playerTexture;
+    private Texture2D mapTexture;
+    private Texture2D wallTexture;
+
+    public GameView(GameModel model)
+    {
+        this.model = model;
+    }
+
+    public void LoadContent(Microsoft.Xna.Framework.Content.ContentManager content)
+    {
+        playerTexture = content.Load<Texture2D>("player");
+        mapTexture = content.Load<Texture2D>("map");
+        wallTexture = content.Load<Texture2D>("wall");
+    }
+        
+
+    public void Draw(SpriteBatch spriteBatch)
+    {
+        spriteBatch.Draw(mapTexture, new Rectangle(0, 0, 800, 600), Color.White);
+        spriteBatch.Draw(playerTexture, new Rectangle((int)model.PlayerPosition.X, (int)model.PlayerPosition.Y, 16, 16), Color.White);
+        foreach (var wall in model.Walls)
+        {
+            spriteBatch.Draw(wallTexture, wall, Color.White);
+        }
+    }
+}
